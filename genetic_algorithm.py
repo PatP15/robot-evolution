e85be694ba5c2c7638bc0c3c3bea1b7ebe1d0b17
Aniv Ray <anivray10@gmail.com>
2023-11-16 21:22:44 -0500

    Added device specificiation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 from display_dog import simulate
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
 class GeneticAlgorithm():
 
     def __init__(self, populationSize, numCenters):
@@ -24,7 +26,7 @@ class GeneticAlgorithm():
         centerLocations[..., 1] = centerLocations[..., 1] * 4
         centerLocations[..., 2] = centerLocations[..., 2] * 2
         centerMaterials = torch.randint(low=1, high=4, size=(self.populationSize, self.numCenters, 1), dtype=torch.float)
-        return centerLocations.to("cuda:0"), centerMaterials.to("cuda:0")
+        return centerLocations.to(device), centerMaterials.to(device)
 
     def evaluate(self):
         return simulate(self.centerLocs, self.centerMats)
